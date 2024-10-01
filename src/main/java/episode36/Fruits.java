766dee278ec629aaff2a187a2f59876ef348c06f
srini <srini@srini-ThinkPad-E15-Gen-2>
2024-10-01 13:42:13 +0530

    Inheritance and List

--- src/main/java/episode36/Fruits.java
+++ src/main/java/episode36/Fruits.java
@@ -1,10 +1,47 @@
 package episode36;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Fruits {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		List<String> list = new ArrayList<String>();
+		list.add("Mango");
+		list.add("Apple");
+		list.add("Mango");
+		list.add("Orange");
+		list.add("Pine");
+		for (String string : list) {
+			System.out.println(string);
+			
+		}
+		System.out.println("-----------");
+		boolean empty = list.isEmpty();
+		System.out.println(empty);
+		System.out.println("-----------");
+		String firstvalue = list.get(0);
+		System.out.println(firstvalue);
+		System.out.println("-------------");
+		
+		boolean contains = list.contains("Pine");
+		System.out.println(contains);
+		
+		list.remove("Apple");
+		System.out.println("---------------");
+		
+		for (String string : list) {
+			System.out.println(string);
+			
+		}
+		Integer total = list.size();
+		System.out.println("Total Size:  " +total);
+		System.out.println("--------------");
+		list.clear();
+		Integer total1 = list.size();
+		System.out.println("Total Size:  " +total1);
 	}
 
 }
